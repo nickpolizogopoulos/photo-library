1e048c3d7ca0b303912af7daf1a9c3b62c358335
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2026-05-22 21:22:48 +0300

    feat: add header navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './shared/header/header';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [
+    Header,
+    RouterOutlet
+  ],
+  template: `
+  
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  
+  `
 })
 export class App {
   protected readonly title = signal('photo-library');
---
+++ src/app/shared/header/header.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  template: `
+  
+    <mat-toolbar class="toolbar">
+      <button
+        mat-button
+        routerLink="/"
+        routerLinkActive="active"
+        [routerLinkActiveOptions]="{ exact: true }"
+      >
+        Photos
+      </button>
+
+      <button
+        mat-button
+        routerLink="/favorites"
+        routerLinkActive="active"
+      >
+        Favorites
+      </button>
+    </mat-toolbar>
+
+  `,
+  styles: `
+  
+    .toolbar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 3rem;
+        height: 4rem;
+        border-bottom: 1px solid rgb(222, 222, 222);
+    }
+
+    .active {
+      font-weight: bold;
+      border: 1px solid rgb(222, 222, 222);
+    }
+  `
+})
+export class Header {}
