3277c76612c85b425873235812e019d646d897ea
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2026-05-22 21:37:46 +0300

    feat: setup routing and basic route components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/photos/photos').then(m => m.Photos)
+    },
+    {
+        path: 'favorites',
+        loadComponent: () => import('./features/favorites/favorites').then(m => m.Favorites)
+    },
+    {
+        path: 'photos/:id',
+        loadComponent: () => import('./features/photo-details/photo-details').then(m => m.PhotoDetails)
+    }
+];
---
+++ src/app/features/photo-details/photo-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-details',
+  imports: [],
+  templateUrl: './photo-details.html',
+  styleUrl: './photo-details.scss',
+})
+export class PhotoDetails {}
