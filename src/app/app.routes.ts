2c7ae1d832558f64205ef23e522a7718c03f9fd4
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2026-05-23 16:44:55 +0300

    feat: add dynamic page titles for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 
+export const getPageTitle = (pageTitle: string) =>
+    `Photo Library - ${pageTitle}`;
+
 export const routes: Routes = [
     {
         path: '',
+        title: getPageTitle('Photos'),
         loadComponent: () => import('./features/photos/photos').then(m => m.Photos)
     },
     {
         path: 'favorites',
+        title: getPageTitle('Favorites'),
         loadComponent: () => import('./features/favorites/favorites').then(m => m.Favorites)
     },
     {
         path: 'photos/:id',
+        title: getPageTitle('Photo'),
         loadComponent: () => import('./features/photo-details/photo-details').then(m => m.PhotoDetails)
     }
 ];
