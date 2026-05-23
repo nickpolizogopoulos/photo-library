6952e83f5341c50c9dac97c70b684976766eff1e
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2026-05-23 16:36:21 +0300

    feat: implement photo details page

--- src/app/features/photo-details/photo-details.ts
+++ src/app/features/photo-details/photo-details.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
+
+import { Favorites as FavoritesService } from '../../services/favorites/favorites'
 
 @Component({
-  imports: [],
-  templateUrl: './photo-details.html',
-  styleUrl: './photo-details.scss',
+  imports: [
+    MatButtonModule,
+    MatIconModule
+  ],
+  template: `
+
+    <section class="flex-center">
+      <img class="display-img" [src]="photo()?.url" [alt]="'photo: ' + photo()?.id"/>
+      <button
+        matButton="filled"
+        class="btn-destructive"
+        (click)="removeFromFavorites()"
+      >
+        <mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="delete"></mat-icon>
+        Remove from favorites
+      </button>
+    </section>
+  `
 })
-export class PhotoDetails {}
+export class PhotoDetails {
+
+  readonly #router = inject(Router);
+  readonly #activatedRouite = inject(ActivatedRoute);
+  readonly #favoritesService = inject(FavoritesService);
+
+  readonly #photoId = this.#activatedRouite.snapshot.paramMap.get('id');
+  protected readonly photo = computed(() =>
+    this.#favoritesService
+      .favorites()
+      .find(photo => photo.id === this.#photoId)
+  );
+
+  protected removeFromFavorites() {
+    if (!this.#photoId)
+      return;
+
+    this.#favoritesService.remove(this.#photoId);
+    this.#router.navigate(['/favorites']);
+  };
+  
+}
